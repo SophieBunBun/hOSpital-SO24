5591f7e6bbbd3d1125fac15a2479f293398f1b6e
Adryll <enderstorm100@gmail.com>
2024-04-14 00:00:58 +0100

    sleep....

--- src/main.c
+++ src/main.c
@@ -106,7 +106,7 @@ void user_interaction(struct data_container* data, struct communication* comm) {
             wait_processes(data);
         }*/
         
-        sleep(0.2);
+        sleep(1);
         printf("\nComando: ");
         scanf("%s", command);
         if (strcmp(command, "ad") == 0) {
