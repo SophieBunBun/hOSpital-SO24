a1c8da018d5592a2aab04232338cc78655173e8c
nunograxinha <72356275+nunograxinha@users.noreply.github.com>
2024-05-10 21:41:10 +0100

    added stats.h

---
+++ include/stats.h
@@ -0,0 +1,29 @@
+/**
+* Grupo SO-028
+* Sofia Santos  fc59804
+* Joao Dias     fc59854
+* Nuno Graxinha fc59855
+*/
+#ifndef STATS_H
+#define STATS_H
+
+#include <stdio.h>
+#include <time.h>
+
+
+// Função para escrever as estatísticas finais no arquivo
+void write_statistics(const char* filename, struct data_container* data);
+
+// Função para escrever as estatísticas dos processos (pacientes, recepcionistas e médicos)
+void write_process_statistics(FILE* file, struct data_container* data);
+
+// Função para escrever as estatísticas das admissões
+void write_admission_statistics(FILE* file, struct data_container* data);
+
+// Função para obter a string de tempo formatada
+char* get_formatted_time();
+
+// Função para converter timespec em segundos
+double get_seconds_from_time(struct timespec ts);
+
+#endif 
